5b26479773eb15bf34fbe885b1e5be8e0ee62082
EfremovPG <pavelefremovmail@mail.ru>
2024-08-05 17:15:42 +0300

    v1 без ошибок, не запускается

---
+++ app/src/main/java/com/example/praporlesson/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.praporlesson
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val userEnterName = findViewById<EditText>(R.id.edt_name).toString()
+        val userEnterPrice = findViewById<EditText>(R.id.edt_price).toString()
+        val button: Button = findViewById<Button>(R.id.btn_calculate)
+        findViewById<Button>(R.id.btn_calculate)?.setOnClickListener {
+            button.setOnClickListener {
+                val action = listOf("Бананы", "Яблоки", "Шоколад")
+                val discount = 0.05
+                val cashRate = 1.5
+                val sumRate = 100
+                val formaterDeci = "%.2f"
+                val formaterInt = "%.0f"
+                val finalPrint =
+                    when {
+                        (userEnterName in action) -> {
+                            (userEnterPrice.toDouble() * discount)
+                        }
+
+                        else -> {
+                            ((Math.floor(userEnterPrice.toDouble() / sumRate)) * cashRate)
+                        }
+                    }
+                val forFormater =
+                    if (finalPrint % 1.0 == 0.toDouble()) {
+                        formaterInt
+                    } else formaterDeci
+                val txtResult: Any = forFormater.format(finalPrint)
+//                findViewById<EditText>(R.id.txt_result)
+            }
+//        txtResult}
+//            val txtResult: Any = fortFormater.format(finalPrint)
+        }
+    }
+}
+//findViewById<EditText>(R.id.txt_result)
